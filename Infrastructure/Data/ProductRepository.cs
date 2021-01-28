10071b8078289f7f5c415a3e757377f883912430
Trương Nguyên <truongnguyen@Truong-Nguyen.local>
2021-01-28 17:53:51 +0700

    End of section 4

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
@@ -26,6 +27,9 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
+            var typeId = 1;
+            var products = _context.Products
+                            .Where(x => x.ProductTypeId == typeId).Include(x => x.ProductType).ToListAsync();
            return await _context.Products
                     .Include(p => p.ProductBrand)
                     .Include(p => p.ProductType)
